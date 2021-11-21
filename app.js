d01840f838fbf7a7dff8330354bb1a7d332f6332
Nick Cardoza <cardoza.nick21@gmail.com>
2021-11-20 19:39:31 -0600

    made site more responsive. videos only play when in specific section of window. new favicon added to files.

--- app.js
+++ app.js
@@ -36,12 +36,33 @@ tl.from("#me-pic", { x: -100, opacity: 0, duration: 1.5 }, "<-2");
 tl.from("#prof-info", { x: -50, opacity: 0, duration: 1.5 }, "<+.5");
 // $(".landing").remove(); this works, but does so immediately. need to chain it to gsap
 
-// start playing the videos when the top of the projects div hits center screen
+// start playing the videos when they are center screen
 ScrollTrigger.create({
-    trigger: ".info",
-    start: "top center",
+    trigger: "#dtw-video",
+    start: "center center",
+    end: "center 20%",
+    // markers: true,
+    onToggle: (self) => {
+        var video = $("#dtw-video video:first-child");
+        if (self.isActive) {
+            video.trigger("play");
+        } else {
+            video.trigger("pause");
+        }
+    },
+});
+ScrollTrigger.create({
+    trigger: "#cc-video",
+    start: "top 60%",
+    // markers: true,
+
     // action: "play pause",
-    onToggle: () => {
-        $("video").trigger("play");
+    onToggle: (self) => {
+        var video = $("#cc-video video:first-child");
+        if (self.isActive) {
+            video.trigger("play");
+        } else {
+            video.trigger("pause");
+        }
     },
 });
